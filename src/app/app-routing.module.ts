17039a11116b79de647d053749d29739fab674f2
montheooo <montheooo@yahoo.fr>
2023-06-11 08:29:57 -0400

    update login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ReactiveFormComponent } from './components/reactive-form/reactive-form.
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'job/:id', component: JobDetailsComponent },
-  { path: 'addjob', component: ReactiveFormComponent }
+  { path: 'addjob', component: ReactiveFormComponent },
+  { path: 'login', component: LoginComponent }
 
 
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit{
+
+  constructor(){
+
+  }
+
+  userEmail!:string;
+  passWord!:string
+
+  ngOnInit(){
+
+  }
+
+  signIn(f:NgForm){
+    console.log(f.value)
+  }
 
 }
